7027894e34472b12c79502e49d4a8fa938f1e656
Meherab Hossain <43495851+meherab-mhk@users.noreply.github.com>
2024-11-23 01:09:08 +0600

    Update firebase.config.js

--- codeBlogsClient/src/firebase/firebase.config.js
+++ codeBlogsClient/src/firebase/firebase.config.js
@@ -8,20 +8,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-//   apiKey: process.env.API_KEY,
-//   authDomain: process.env.AUTH_DOMAIN,
-//   projectId: process.env.PROJECT_ID,
-//   storageBucket: process.env.STORAGE_BUCKET,
-//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
-//   appId: process.env.APP_ID
-
-    apiKey: "AIzaSyALmhoS8QzDXE1DkHtxp06AlL4XpwvHrf4",
-    authDomain: "code-blogs-8ff17.firebaseapp.com",
-    projectId: "code-blogs-8ff17",
-    storageBucket: "code-blogs-8ff17.firebasestorage.app",
-    messagingSenderId: "587349843997",
-    appId: "1:587349843997:web:2521dd70272504e04966f4"
-
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 // Initialize Firebase
